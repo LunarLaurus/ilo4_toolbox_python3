3a35059a02f8170b430d1211a96c711f37bfe8b6
LunarLaurus <121956871+LunarLaurus@users.noreply.github.com>
2025-11-11 16:13:30 +0000

    Refactor secinfo4.py for updated IDA API usage

--- scripts/iLO4/secinfo4.py
+++ scripts/iLO4/secinfo4.py
@@ -4,12 +4,12 @@ from ctypes import *
 
 # struct MAP_ENTRY
 # {
-  # MAP_ENTRY *next;
-  # int ptr_name;
-  # int base;
-  # int size;
-  # int access;
-  # int field_14;
+#   MAP_ENTRY *next;
+#   int ptr_name;
+#   int base;
+#   int size;
+#   int access;
+#   int field_14;
 # };
 
 class MapEntry(LittleEndianStructure):
@@ -24,23 +24,22 @@ class MapEntry(LittleEndianStructure):
     ]
 
     def to_str(self, ea):
-        max_len = idaapi.get_max_ascii_length(ea, ASCSTR_C, 0)
-        idaapi.make_ascii_string(ea, max_len, ASCSTR_C)
-        return idc.GetString(ea, max_len, ASCSTR_C)
+        max_len = idaapi.get_max_ascii_length(ea, idaapi.ASCSTR_C, 0)
+        idaapi.make_ascii_string(ea, max_len, idaapi.ASCSTR_C)
+        return idc.get_strlit_contents(ea, max_len, idaapi.ASCSTR_C).decode("utf-8")
 
     def dump(self):
-        print "  %20s - base 0x%08x - size 0x%08x - access : 0x%x" % (
-            self.to_str(self.name_addr), self.base, self.size, self.access)
+        print("  {:20s} - base 0x{:08X} - size 0x{:08X} - access : 0x{:X}".format(
+            self.to_str(self.name_addr), self.base, self.size, self.access))
 
     def name(self):
-        return "%s" % self.to_str(self.name_addr)
+        return "{}".format(self.to_str(self.name_addr))
 
 
+print("> parsing .secinfo entries:\n")
 
-print "> parsing .secinfo entries:\n"
-
-sid  = GetStrucIdByName('MAP_ENTRY')
-ssize = idc.GetStrucSize(sid)
+sid = idc.get_struc_id_by_name('MAP_ENTRY')
+ssize = idc.get_struc_size(sid)
 
 # for v 2.44.7 19-Jul-2016
 SEC_INFO_LINKED_LIST = 0x200B6A3C
@@ -49,9 +48,9 @@ ea = SEC_INFO_LINKED_LIST
 entries = []
 
 while True:
-    idc.MakeUnknown(ea, ssize, idc.DOUNK_DELNAMES)
-    idaapi.doStruct(ea, ssize, sid);
-    buf = idc.GetManyBytes(ea, ssize)
+    idc.del_items(ea, idc.DOUNK_DELNAMES, ssize)
+    idaapi.doStruct(ea, ssize, sid)
+    buf = idc.get_bytes(ea, ssize)
 
     entry = MapEntry.from_buffer_copy(buf)
     entries.append(entry)
@@ -63,30 +62,29 @@ while True:
     ea = entry.next
 
 
-print "\n> setting-up segments:\n"
+print("\n> setting-up segments:\n")
 
 for entry in [e for e in entries if e.size != 0]:
-    make_segment(entry.base, entry.size, entry.name())
-
+    idaapi.add_segm(0, entry.base, entry.base + entry.size, entry.name(), "DATA")
 
 
 bss_list = [e for e in entries if e.name() == '.secinfo']
 
 if len(bss_list) == 1:
-    print "\n> parsing BSS entries:\n"
+    print("\n> parsing BSS entries:\n")
 
     secinfo = bss_list[0]
     ea = secinfo.base
-    sid  = GetStrucIdByName('BSS_ENTRY')
-    ssize = idc.GetStrucSize(sid)
+    sid = idc.get_struc_id_by_name('BSS_ENTRY')
+    ssize = idc.get_struc_size(sid)
 
     while True:
-        idc.MakeUnknown(ea, ssize, idc.DOUNK_DELNAMES)
-        idaapi.doStruct(ea, ssize, sid);
-        buf = idc.GetManyBytes(ea, ssize)
+        idc.del_items(ea, idc.DOUNK_DELNAMES, ssize)
+        idaapi.doStruct(ea, ssize, sid)
+        buf = idc.get_bytes(ea, ssize)
         bsse = BssEntry.from_buffer_copy(buf)
 
-        elist =  [e for e in entries if (e.base == bsse.base and e.size != 0)]
+        elist = [e for e in entries if (e.base == bsse.base and e.size != 0)]
         if len(elist) == 1:
             bsse.dump(elist[0].name())
         else:
@@ -97,4 +95,4 @@ if len(bss_list) == 1:
             break
 
 
-print "\n[+] job done captain!\n"
+print("\n[+] job done captain!\n")
