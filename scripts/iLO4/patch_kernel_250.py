90a4a5eb6888ff6b91a2ba5ed4cd5c8f82650106
LunarLaurus <121956871+LunarLaurus@users.noreply.github.com>
2025-11-11 16:37:38 +0000

    Update script to use Python 3 and improve readability

--- scripts/iLO4/patch_kernel_250.py
+++ scripts/iLO4/patch_kernel_250.py
@@ -1,27 +1,39 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import sys
 
 # Patch signature check : BEQ XX -> B XX
-PATCH = {"offset": 0x1FB1C, "size": 4, "prev_data": "0400000A", "patch": "040000EA"}
+PATCH = {
+    "offset": 0x1FB1C,
+    "size": 4,
+    "prev_data": "0400000A",
+    "patch": "040000EA"
+}
 
 if len(sys.argv) < 2:
-    print "usage: %s <kernel.bin>" % sys.argv[0]
+    print("usage: {} <kernel.bin>".format(sys.argv[0]))
     sys.exit(1)
 
+# read kernel
 with open(sys.argv[1], "rb") as f:
     data = f.read()
 
-check_data = data[PATCH["offset"]:PATCH["offset"]+PATCH["size"]]
-if check_data != PATCH["prev_data"].decode("hex"):
-    print "[-] Error, bad file content at offset %x" % PATCH["offset"]
-    print "\t Expected:\t%s" % PATCH["prev_data"]
-    print "\t Got:\t%s" % check_data.encode("hex")
+# verify existing bytes
+check_data = data[PATCH["offset"]:PATCH["offset"] + PATCH["size"]]
+expected_bytes = bytes.fromhex(PATCH["prev_data"])
+if check_data != expected_bytes:
+    print("[-] Error, bad file content at offset 0x{:X}".format(PATCH["offset"]))
+    print("\tExpected:\t{}".format(PATCH["prev_data"]))
+    print("\tGot:     \t{}".format(check_data.hex()))
     sys.exit(1)
 
-data = data[:PATCH["offset"]] + PATCH["patch"].decode("hex") + data[PATCH["offset"]+PATCH["size"]:]
+# apply patch
+patched_bytes = bytes.fromhex(PATCH["patch"])
+data = data[:PATCH["offset"]] + patched_bytes + data[PATCH["offset"] + PATCH["size"]:]
 
-with open(sys.argv[1] + ".patched", "wb") as f:
+# write patched file
+out_file = sys.argv[1] + ".patched"
+with open(out_file, "wb") as f:
     f.write(data)
 
-print "[+] Patch applied to %s.patched" % sys.argv[1]
+print("[+] Patch applied to {}".format(out_file))
